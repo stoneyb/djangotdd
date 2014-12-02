3ff36ced14ecbc4f0e22a74de526f62d5462a87f
Tom Stoneberg <tstoneb@gmail.com>
2014-12-02 13:57:41 -0700

    new URL, view and tmeplate to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view_list'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
