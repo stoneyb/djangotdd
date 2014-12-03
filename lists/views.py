472a700487455fd8a0492e917db1041ffc1afb22
Tom Stoneberg <tstoneb@gmail.com>
2014-12-03 13:15:51 -0700

    Refactor list view to handle new item POSTs

--- lists/views.py
+++ lists/views.py
@@ -8,6 +8,9 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id))
     items = Item.objects.filter(list=list_)
     return render(request, 'lists/list.html', {'list': list_})
 
@@ -23,7 +26,3 @@ def new_list(request):
         return render(request, 'lists/home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id))
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id))
