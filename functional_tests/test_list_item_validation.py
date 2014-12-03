ba841f99036e1053f994f02908ef0cf97b0ab708
Tom Stoneberg <tstoneb@gmail.com>
2014-12-03 16:05:02 -0700

    use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -27,7 +27,7 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she cann correct it by filling some text in
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.get_item_input_box().send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
 
