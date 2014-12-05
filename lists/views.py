75ede08665d31c8b5707c24ef45d7a09bb7ea67e
Tom Stoneberg <tstoneb@gmail.com>
2014-12-05 11:26:22 -0700

    url, placeholder view, and first-cut templates for my_lists

--- lists/views.py
+++ lists/views.py
@@ -26,3 +26,5 @@ def new_list(request):
     else:
         return render(request, 'lists/home.html', {"form": form})
 
+def my_lists(request, email):
+    return render(request, 'lists/my_lists.html')
